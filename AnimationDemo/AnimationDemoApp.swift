22afec645e7f24afb6e3032e71209df2acfa0556
Abhishek Thapliyal <thapliyal@fueled.com>
2022-10-10 18:56:13 +0530

    files

---
+++ AnimationDemo/AnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationDemoApp.swift
+//  AnimationDemo
+//
+//  Created by Abhishek Thapliyal on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationDemo/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  AnimationDemo
+//
+//  Created by Abhishek Thapliyal on 10/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+
+	private var animationView: some View {
+		HStack {
+
+		}
+		.padding(.horizontal, 50)
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
