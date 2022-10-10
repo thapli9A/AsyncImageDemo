fae286178aa25619d81ef6dc36f095c432f85d7e
Abhishek Thapliyal <thapliyal@fueled.com>
2022-10-10 19:01:53 +0530

    files

--- AnimationDemo/ContentView.swift
+++ AnimationDemo/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+
+	let url = "https://raw.githubusercontent.com/thapli9A/AsyncImageDemo/main/Images/img101.png"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		NavigationView {
+			VStack {
+				Spacer()
+				NavigationLink("PUSH MRE") {
+					NewView()
+				}
+				.buttonStyle(.plain)
+				animationView
+				Spacer()
+			}
+		}
     }
 
 	private var animationView: some View {
 		HStack {
-
+			AsyncImage(url: URL(string: url)!)
 		}
 		.padding(.horizontal, 50)
 	}
@@ -25,4 +37,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct NewView: View {
+	var body: some View {
+		Text("CHECk")
+	}
 }
