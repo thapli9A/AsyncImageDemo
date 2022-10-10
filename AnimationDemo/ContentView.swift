0ebcfcd976e89d9ac1a62f36e4286462344f4403
Abhishek Thapliyal <thapliyal@fueled.com>
2022-10-10 19:11:20 +0530

    files

--- AnimationDemo/ContentView.swift
+++ AnimationDemo/ContentView.swift
@@ -9,25 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
 
+	@State var dummyText = "asddadas"
 	let url = "https://raw.githubusercontent.com/thapli9A/AsyncImageDemo/main/Images/img101.png"
 
     var body: some View {
 		NavigationView {
-			VStack {
-				Spacer()
-				NavigationLink("PUSH MRE") {
-					NewView()
+			parentView
+				.navigationBarTitleDisplayMode(.inline)
+				.onAppear {
+					dummyText += "\(dummyText.count)"
 				}
-				.buttonStyle(.plain)
-				animationView
-				Spacer()
-			}
 		}
     }
+
+	private var parentView: some View {
+		VStack {
+			Spacer()
+			NavigationLink("PUSH MRE") {
+				NewView()
+			}
+			.buttonStyle(.plain)
+			Text(dummyText)
+			animationView
+			Spacer()
+		}
+	}
 
 	private var animationView: some View {
 		HStack {
 			AsyncImage(url: URL(string: url)!)
+				.aspectRatio(contentMode: .fit)
+				.frame(width: 300, height: 600)
+				.clipShape(Rectangle())
 		}
 		.padding(.horizontal, 50)
 	}
